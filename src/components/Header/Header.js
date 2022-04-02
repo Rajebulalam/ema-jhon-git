cfeffab2ff4309dfb84fadd8007f38c6377b5046
unknown <rajebul.alam17@gmail.com>
2022-04-02 15:03:45 +0600

    Header Part Commited

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <div>
+                <img src={logo} alt="logo-img" />
+            </div>
+            <div>
+                <Link to='/'>Shop</Link>
+                <Link to='/orders'>Orders</Link>
+                <Link to='./inventory'>Inventory</Link>
+                <Link to='./About'>About</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
