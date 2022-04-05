748a21d7d82e11c06ad00fbc60f8922deca3abae
unknown <rajebul.alam17@gmail.com>
2022-04-05 20:08:15 +0600

    Home Page Created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Home.css';
+import home from '../../images/group.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Rajebul Alam Khokan</h2>
+        <div className='home-container'>
+            <div>
+                <h4>Sale up to 70% off</h4>
+                <h2>New Collection For Fall</h2>
+                <p>Discover all the new arrivals of ready-to-wear collection.</p>
+                <Link to='/shop'>SHOP NOW</Link>
+            </div>
+            <div>
+                <img src={home} alt="home-img" />
+            </div>
         </div>
     );
 };
