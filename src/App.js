bd87c85916b81fe5c288fa6f078cef9fddecf522
unknown <rajebul.alam17@gmail.com>
2022-04-13 20:09:50 +0600

    About and Inventory Page Created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Header from "./components/Header/Header";
 import Home from './components/Home/Home';
+import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 
@@ -13,6 +15,8 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Inventory.css';
+
+const Inventory = () => {
+    return (
+        <div className='inventory-container'>
+            <h2>Inventory Product</h2>
+        </div>
+    );
+};
+
+export default Inventory;
