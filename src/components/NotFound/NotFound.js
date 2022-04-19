fdfdef0bd658f73113dc866c7a5ea7965c4b58ee
unknown <rajebul.alam17@gmail.com>
2022-04-19 23:45:11 +0600

    Not Found Page Designed

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div className='notfound-container'>
-            <h2>Sorry Nothing Found!</h2>
+        <div className='notFound-container'>
+            <Helmet>
+                <title> NotFound - Ema Jhon Shop </title>
+            </Helmet>
+            <h1>Opps!</h1>
+            <h2>404 - PAGE NOT FOUND</h2>
+            <p>The page you are looking for might have been removed</p>
+            <p>had its name changed or is temporarily unavailable</p>
+            <Link to="/">GO TO HOME PAGE</Link>
         </div>
     );
 };
